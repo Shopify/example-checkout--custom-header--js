8569e04c8ed1ac82f7a1c822fd4baa16f4bcdebb
Greg Craft <greg.craft@shopify.com>
2024-01-26 10:32:20 -0600

    Renaming Header to Extension to match the react tutorial

--- extensions/custom-header/src/ThankYou.js
+++ extensions/custom-header/src/ThankYou.js
@@ -1,6 +1,6 @@
 import { extension } from "@shopify/ui-extensions/checkout";
-import renderHeader from './Header.js';
+import renderExtension from './Extension.js';
 
 export default extension("purchase.thank-you.header.render-after", (root) => {
-  renderHeader({root});
+  renderExtension({root});
 });
