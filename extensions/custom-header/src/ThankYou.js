ca0587e574543ae960de72c602d44c689ed27cdd
Greg Craft <greg.craft@shopify.com>
2024-01-26 12:01:40 -0600

    Updating extension readme and minor code formatting

--- extensions/custom-header/src/ThankYou.js
+++ extensions/custom-header/src/ThankYou.js
@@ -1,6 +1,7 @@
-import { extension } from "@shopify/ui-extensions/checkout";
+import {extension} from '@shopify/ui-extensions/checkout';
+
 import renderExtension from './Extension.js';
 
-export default extension("purchase.thank-you.header.render-after", (root) => {
+export default extension('purchase.thank-you.header.render-after', (root) => {
   renderExtension({root});
 });
